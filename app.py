50d56a1d5bce72cc72bb223b778ccbd2a1935702
AsmaeAitBensaid <asmaeaitbensaid645@gmail.com>
2026-01-31 15:59:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,26 @@ app = Flask(__name__)
 
 # Charger les données
 data = pd.read_csv("data.csv")
+data["user_id"] = data["user_id"].astype(int)
+
 
 @app.route("/")
 def home():
     return send_from_directory(".", "index.html")
 
 @app.route("/recommend", methods=["POST"])
+@app.route("/recommend", methods=["POST"])
 def recommend():
-    user_id = request.json.get("user_id")
+    user_id = int(request.json.get("user_id"))
 
-    # Films vus par l'utilisateur
     seen = data[data["user_id"] == user_id]["movie_title"]
 
-    # Films populaires
-    popular = (
-        data["movie_title"]
-        .value_counts()
-        .index
-    )
+    popular = data["movie_title"].value_counts().index
 
-    # Recommandations
     recommendations = [m for m in popular if m not in seen][:5]
 
     return jsonify({"recommendations": recommendations})
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
